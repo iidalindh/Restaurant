1a18fe380d0466e1f41d35a91e9a3383e71cc7a5
Jesper Karlsson <jesper.karlsson@medieinstitutet.se>
2021-08-23 11:26:22 +0200

    Sebbe kollar på fel

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Login } from "./components/login/Login";
 import { Register } from "./components/register/Register";
 import { LandingPage } from "./components/landingPage/LandingPage";
+import { Booking } from "./components/booking/Booking";
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
         </Route>
         <Route path="/register">
           <Register />
+        </Route>
+        <Route path="/booking">
+          <Booking />
         </Route>
         <Route path="/admin">
           //Här visar du komponenten som ska visas på den routen
--- Client/src/components/booking/Booking.tsx
+++ Client/src/components/booking/Booking.tsx
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import axios from 'axios';
+import styled from 'styled-components';
+
+const Button = styled.button`
+    color: blue;
+`
 
 export const Booking = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const [dateValue, setDateValue] = useState(new Date());
+
+  function changeDate(e: any) {
+    setDateValue(e);
+  }
+
+  function onSubmit() {
+    const dataToSend = {
+        date: dateValue,
+        time: 18,
+        numberOfGuests: 4,
+        customerName: "Ida",
+        customerEmail: "bajs@email.com"
+    }
+
+    axios.post('http://localhost:8000/')
+  }
+
+  
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <Calendar onChange={changeDate} value={dateValue} />
+        <button
+          onClick={() => {
+            console.log(dateValue.toLocaleDateString());
+          }}
+        >
+          Logga
+        </button>
+        <Button>Hej</Button>
+      </form>
+    </div>
+  );
+};

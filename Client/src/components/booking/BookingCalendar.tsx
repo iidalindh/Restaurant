eddef24ccf8db42845cfc05ca53853042339d870
jesperkarlsson42 <jesper.karlsson@medieinstitutet.se>
2021-08-31 15:00:53 +0200

    handle response in frontend

--- Client/src/components/booking/BookingCalendar.tsx
+++ Client/src/components/booking/BookingCalendar.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useState } from "react";
 import Calendar from "react-calendar";
 import { IBooking } from "./Booking";
@@ -17,10 +18,39 @@ export const BookingCalendar = (props: IBookingCalendarProps) => {
     props.pickDate(e.toLocaleDateString());
   }
 
+  async function getAvailableTables() {
+    const date = dateValue.toLocaleDateString();
+    const data = {
+      numberOfGuests: 9,
+      date: date
+    }
+    const res = await axios.post ("http://localhost:8000/booking/getAvailableTables", data);
+    console.log(res.data);
+
+    for (let ida = 0; ida < res.data.length; ida++) {
+      if(res.data[ida].time === 18) {
+        if(res.data[ida].availableTables === false) {
+          console.log('disable knapp jäveln för 18'); 
+        } else {
+          console.log('varmt välkommen klockan 18 motherfucker');
+          
+        }
+      } else if(res.data[ida].time === 21) {
+        if(res.data[ida].availableTables === false) {
+          console.log('disable knapp jäveln för 21');
+        } else {
+          console.log('varmt välkommen klockan 21 motherfucker');
+        }
+      } 
+      
+    }
+    
+  }
 
   function runFunctions(e : any) {
     changeDate(e);
     selectDate(e);
+    getAvailableTables();
   }
 
   return (

5c4f323324ac38d923ff2731e902dec9167ea517
iidalindh <idalindh97@gmail.com>
2021-09-06 14:50:43 +0200

    Adding popup for GDPR rules

--- Client/src/components/booking/BookingDetails.tsx
+++ Client/src/components/booking/BookingDetails.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import { BookingGdprPopup } from "./BookingGdprPopup";
 
 export interface IBookingDetailsProps {
   date: string;
@@ -16,6 +17,7 @@ export const BookingDetails = (props: IBookingDetailsProps) => {
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [checked, setChecked] = useState(false);
+  const [popup, setPopup] = useState(false);
 
   useEffect(() => {
     formSubmit();
@@ -107,23 +109,94 @@ export const BookingDetails = (props: IBookingDetailsProps) => {
             ></Input>
             <GDPRConfirmDiv>
               {checked ? (
-                <button
+                <CheckButton
                   onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
                     toggleCheckbox(e);
                   }}
                 >
                   <i className="far fa-check-square"></i>
-                </button>
+                </CheckButton>
               ) : (
-                <button
+                <CheckButton
                   onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
                     toggleCheckbox(e);
                   }}
                 >
                   <i className="far fa-square"></i>
-                </button>
+                </CheckButton>
               )}
-              <label htmlFor="confirmGDPR"> Jag accepterar villkoren</label>
+              <label htmlFor="confirmGDPR">
+                Jag accepterar{" "}
+                <a
+                  onClick={() => {
+                    setPopup(true);
+                  }}
+                >
+                  villkoren
+                </a>
+              </label>
+              <BookingGdprPopup trigger={popup}>
+                <h3>Behandling av personuppgifter</h3>
+                <p>
+                  Behandling av personuppgifterHappy Consulting i Stockholm AB,
+                  är personuppgiftsansvarig för de uppgifter du som kund lämnar
+                  till Restaurang Athena org.nr 556361-9912. Vi behandlar
+                  personuppgifter i enlighet med Personuppgiftslagen (PUL). Den
+                  25 maj 2018 ersätts PuL av Dataskyddsförordningen (EU)
+                  2016/679.
+                </p>
+                <h4>Personuppgifter som vi samlar in och behandlar</h4>
+                <p>
+                  Vi samlar endast in Personuppgifter som är relevanta för din
+                  bokning. Den information som kan komma att samlas in är t.ex.
+                  uppgifter om namn och e-postadress.
+                </p>
+                <h5>Så här använder vi dina personuppgifter</h5>
+                <p>Restaurang Athena använder Personuppgifter för att:</p>
+                <ul>
+                  <li>bearbeta dina bokningar och beställningar.</li>
+                  <li>
+                    * kontakta dig via t.ex. e-post för att avisera om din
+                    bokningsstatus eller information som ansluter till din
+                    bokning före, under och efter din vistelse,diagnostisera
+                    fel, optimera tekniken och kunna kontakta dig i händelse av
+                    problem med en bokning.
+                  </li>
+                </ul>
+                <p>
+                  Du har enligt personuppgiftslagen möjlighet att utan kostnad
+                  en gång per kalenderår få information om vår behandling av
+                  dina uppgifter och kan när som helst begära rättning av
+                  felaktiga uppgifter. Om du inte vill att dina uppgifter skall
+                  finnas kvar i vårt register, vänligen meddela oss detta per
+                  e-post (athenarestaurang@yahoo.com)
+                </p>
+                <h5>Utskick av information</h5>
+                <p>
+                  När du gjort en bokning hos oss använder vi dina
+                  Personuppgifter för att skicka bokningsbekräftelse, viktig
+                  information om den kommande vistelsen och erbjudanden kopplade
+                  till vistelsen. Sådana utskick sker till den e-postadress som
+                  du lämnat i samband med bokning. Efter vistelsen kan vi också
+                  skicka e-post där vi tackar för besöket eller be att se över
+                  kvarglömda saker.
+                </p>
+                <h5>Lagring och överföring av personuppgifter</h5>
+                <p>
+                  Uppgifterna lagras inte längre tid än två (2) år efter det att
+                  kundförhållandet upphört. Vi kan lämna ut Personuppgifter till
+                  tredje man, såsom t.ex. polis eller annan myndighet, om det
+                  rör utredning av brott eller om vi annars är skyldiga att
+                  lämna ut sådan uppgift med stöd av lag eller myndighetsbeslut.
+                </p>
+                <ClosePopup
+                  onClick={() => {
+                    setPopup(false);
+                  }}
+                >
+                  <i className="fas fa-times"></i>
+                </ClosePopup>
+              </BookingGdprPopup>
             </GDPRConfirmDiv>
           </Form>
         </ContactInfo>
@@ -204,4 +277,28 @@ const DetailsPTag = styled.p`
 
 const GDPRConfirmDiv = styled.div`
   display: flex;
+
+  label {
+    a {
+      color: blue;
+      text-decoration: underline;
+    }
+  }
+`;
+
+const CheckButton = styled.button`
+  border: none;
+  background-color: white;
+`;
+
+const ClosePopup = styled.div`
+  position: absolute;
+  top: 16px;
+  right: 16px;
+  cursor: pointer;
+
+  i {
+    color: blue;
+    font-size: 1.3rem;
+  }
 `;
---
+++ Client/src/components/booking/BookingGdprPopup.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+interface IBookingGdprPopupProps {
+  children: React.ReactNode;
+  trigger: boolean;
+}
+
+export const BookingGdprPopup = (props: IBookingGdprPopupProps) => {
+  console.log(props.trigger);
+
+  return (
+    <>
+      {props.trigger ? (
+        <PopupDiv>
+          <PopupInnerDiv>{props.children}</PopupInnerDiv>
+        </PopupDiv>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+};
+
+const PopupDiv = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.2);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const PopupInnerDiv = styled.div`
+  position: relative;
+  padding: 32px;
+  width: 100%;
+  max-width: 640px;
+  background-color: white;
+  max-height: 400px;
+  overflow-y: scroll;
+
+  //Desktop
+  @media (min-width: 1025px) {
+    max-height: 500px;
+  }
+`;

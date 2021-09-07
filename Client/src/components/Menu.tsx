86b91b9786ea1531c2fe9a24b5f13f71c7da9c1c
jesperkarlsson42 <jesper.karlsson@medieinstitutet.se>
2021-09-07 11:01:55 +0200

    added menu component

--- Client/src/components/Menu.tsx
+++ Client/src/components/Menu.tsx
@@ -1,13 +1,31 @@
-import React from 'react'
-import { Footer } from './footer/Footer'
-import { Navbar } from './navbar/Navbar'
+import React from "react";
+import styled from "styled-components";
+import { Footer } from "./footer/Footer";
+import { Navbar } from "./navbar/Navbar";
 
 export const Menu = () => {
-    return (
-        <>
-            <Navbar />
-           <p>Menu</p> 
-           <Footer/>
-        </>
-    )
-}
+  return (
+    <>
+      <Navbar />
+      <ImgDiv>
+        <h1>Meny</h1>
+        <img src="/Meny.png" alt="Menu"></img>
+      </ImgDiv>
+      <Footer />
+    </>
+  );
+};
+
+const ImgDiv = styled.div`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    height: 90vh;
+    margin-bottom: 2rem;
+  }
+`;
